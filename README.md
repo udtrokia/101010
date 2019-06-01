e068f3b94f85d8d1259cdeafa563e0860d97cb11
clearloop <udtrokia@gmail.com>
2019-06-01 14:12:17 +0800

    click and fliping

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 
 [0]: https://web.cs.ucdavis.edu/~amenta/s19/flashcard2.html
+
--- flipcard/jsx/index.jsx
+++ flipcard/jsx/index.jsx
@@ -2,20 +2,58 @@
    This flipcard component is based on the flipcard component by
    Alex Devero, at:
    
-      https://reactjsexample.com/react-flipping-card-with-tutorial/
+   https://reactjsexample.com/react-flipping-card-with-tutorial/
 
    It was modified for ECS 162 by Nina Amenta, May 2019.
-*/
+ */
 
 
 const cardContainer = document.querySelector('.react-card');
 
+const vbs = [{
+  answer: 'starting',
+  riddle: 'What 8 letter word can have a letter taken away and it still makes a word. Take another letter away and it still makes a word. Keep on doing that until you have one letter left. What is the word?'
+}, {
+  answer: 'Racecar',
+  riddle: 'What 7 letter word is spelled the same way backwards and forewards?'
+}, {
+  answer: 'NOON',
+  riddle: 'What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?'
+}, {
+  answer: 'SWIMS',
+  riddle: 'What 5 letter word typed in all capital letters can be read the same upside down?'
+}, {
+  answer: 'Alphabet',
+  riddle: 'What word contains all of the twenty six letters?'
+}, {
+  answer: 'Ton',
+  riddle: 'Foward I am heavy, but backward I am not. What am I?',
+}, {
+  answer: 'Dozens',
+  riddle: 'I am six letters. When you take one away I am twelve. What am I?'
+}, {
+  answer: 'Queue',
+  riddle: 'What English word retains the same pronunciation, even after you take away four of its five letters?'
+}, {
+  answer: 'Heroine',
+  riddle: 'There is a word in the English language in which the first two letters signify a male, the first three letters signify a female, the first four signify a great man, and the whole word, a great woman. What is the word?'
+}];
+
 // React component for form inputs
 class CardInput extends React.Component {
-  render() {
+  state = { text: '' }
+
+  render(props) {
     return(
       <fieldset>
-        <input name={this.props.name} id={this.props.id} type={this.props.type || 'text'} placeholder={this.props.placeholder} required />
+        <input
+	  onChange={this.props.hc.bind(this)}
+	  value={this.props.value}
+	  name={this.props.name}
+	  id={this.props.id}
+	  type={this.props.type || 'text'}
+	  placeholder={this.props.placeholder} required
+	/>
       </fieldset>
     )
   }
@@ -26,20 +64,24 @@ class CardTextarea extends React.Component {
   render() {
     return(
       <fieldset>
-        <textarea name={this.props.name} id={this.props.id} placeholder={this.props.placeholder} required ></textarea>
+        <textarea
+	  name={this.props.name}
+	  id={this.props.id}
+	  placeholder={this.props.placeholder} required >
+	</textarea>
       </fieldset>
     )
   }
 }
 
-
 // React component for the front side of the card
 class CardFront extends React.Component {
   render(props) {
     return(
-      <div className='card-side side-front'>
-         <div className='card-side-container'>
-              <h2 id='trans'>{this.props.text}</h2>
+      <div className={`card-side ${this.props.sfs}`}>
+        <div className='card-side-container'>
+	  <h2 id='trans'>{this.props.text}</h2>
+	  <CardInput hc={this.props.hc} value={this.props.value}  />
         </div>
       </div>
     )
@@ -51,8 +93,8 @@ class CardBack extends React.Component {
   render(props) {
     return(
       <div className='card-side side-back'>
-         <div className='card-side-container'>
-              <h2 id='congrats'>{this.props.text}</h2>
+        <div className='card-side-container'>
+          <h2 id='congrats'>{this.props.text}</h2>
         </div>
       </div>
     )
@@ -61,14 +103,92 @@ class CardBack extends React.Component {
 
 // React component for the card (main component)
 class Card extends React.Component {
+  state = {
+    vbs: vbs,
+    ptr: 0,
+    text: '',
+    answer: vbs[0].answer,
+    sfs: '',
+    cbs: '',
+    seen: 0,
+    correct: 0,
+    user: 'David Bowie'
+  }
+
+  handleChange(e) {
+    /* check if correct */
+    if (
+      e.target.value == vbs[this.state.ptr].answer
+	&& this.state.answer !== 'Correct!'
+    ) {
+      this.setState({
+	answer: 'Correct!'
+      });
+    }
+
+    /* if correct after */
+    if (
+      this.state.answer == 'Correct!'
+	&& e.target.value !== vbs[this.state.ptr].answer
+    ) {
+      this.setState({
+	answer: vbs[this.state.ptr].answer
+      })
+    }
+    
+    this.setState({ text: e.target.value })
+  }
+
+  handleFlip(e) {
+    if (e.key == 'Enter') {
+      if (this.state.answer == 'Correct!') {
+	this.setState({
+	  correct: this.state.corrent + 1
+	})
+      }
+      this.setState({
+	sfs: 'side-front',
+	cbs: 'card-body',
+	seen: this.state.seen + 1
+      })
+      setTimeout(() => {
+	this.setState({
+	  sfs: 'hidden',
+	  cbs: 'hidden',
+	  text: ''	  
+	})
+	if (this.state.ptr <= 8) {
+	  this.setState({ ptr: this.state.ptr + 1 })
+	}
+      }, 2000)
+      setTimeout(() => {
+	this.setState({
+	  sfs: '',
+	  cbs: '',
+	  answer: vbs[this.state.ptr].answer
+	})	
+      }, 3000)
+    }
+  }
+  
   render() {
     return(
-      <div className='card-container'>
-        <div className='card-body'>
-          <CardBack text="Correct!" />
-
-          <CardFront text="Volare" />
-        </div>
+      <div style={{margin: '0 2rem'}}>
+	<div className='card-container' onKeyPress={this.handleFlip.bind(this)}>
+          <div className={this.state.cbs}>
+	    <CardFront
+	      text={vbs[this.state.ptr].riddle}
+	      value={this.state.text}
+	      hc={this.handleChange.bind(this)}
+	      sfs={this.state.sfs}
+	    />
+            <CardBack text={this.state.answer} />
+          </div>
+	</div>
+	<div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+	  <div>Seen: {this.state.seen} <br/> Correct: {this.state.correct} </div>
+	  <div style={{textAlign: 'right'}}>{this.state.user}</div>
+	</div>
       </div>
     )
   }
