987572df6e299442c41cf54f393246be7eb9b51f
clearloop <udtrokia@gmail.com>
2019-06-08 13:24:06 +0800

    dyn score

--- d3/index.js
+++ d3/index.js
@@ -1,46 +1,58 @@
-/* csv */
-function csv(handler) {
-  d3.csv("data.csv", (err, data) => {
-    let group_a = [];
-    let group_b = [];
-    let group_c = [];
-    let group_d = [];
-    let group_e = [];
+function mouseover(mode) {
+  switch(mode) {
+    case 'groupData':
+      return function() {
+	
+      }
+      break;
+    default:
+      return;
+  }
+}
 
-    let standard = [];
-    let fr = [];
-    
+
+/* csv */
+function csv(handler, options) {
+  d3.csv("data.csv", (err, data) => {
+    let groupData = {};
+    let lunchData = {};
+    let eduData = {};
+    let courseData = {};
     data.map(e => {
       let re = e['race/ethnicity'];
-      if (re.match(/A/)) {
-	group_a.push(e);
-      } else if (re.match(/B/)) {
-	group_b.push(e);
-      } else if (re.match(/C/)) {
-	group_c.push(e);
-      } else if (re.match(/D/)) {
-	group_d.push(e);
-      } else if (re.match(/E/)) {
-	group_e.push(e);
-      } else {
-	console.log('no match: ', e);
-      }
+      if (!groupData[re]) { groupData[re] = [] }
+      groupData[re].push(e);
 
       let lunch = e['lunch'];
-      if (lunch.match(/free/)) {
-	fr.push(e);
-      } else {
-	standard.push(e);
-      }
+      if (!lunchData[lunch]) { lunchData[lunch] = []}
+      lunchData[lunch].push(e);
+
+      let edu = e['parental level of education'];
+      if (!eduData[edu]) { eduData[edu] = []}
+      eduData[edu].push(e);
+
+      let course = e['test preparation course'];
+      if (!courseData[course]) { courseData[course] = []}
+      courseData[course].push(e);
     });
 
-    let groups = [
-      group_a, group_b, group_c, group_d, group_e
-    ];
+    let dataset = {
+      groupData,
+      lunchData,
+      eduData,
+      courseData
+    };
 
-    let lunch = [ standard, fr ];
-    
-    handler({ groups, lunch });
+    if (options.type == 'axis') {
+      if (options.init) {
+	let mode = Object.keys(dataset[options.data])[0];
+	handler(dataset[options.data][mode]);
+      } else {
+	handler(dataset[options.data][options.mode]);
+      }
+    } else {
+      handler(dataset[options.data], options.chart);
+    }
   });
 }
 
@@ -83,16 +95,18 @@ class Pie {
     return ret;
   }
 
-  score(data) {
+  static draw(data, chart) {
     let raw = data;
-    data = data.groups.map(e => e.length);
-
+    let options = Object.keys(data);
+    
     let {
       svg, pie, arc, labelArc, color
     } = Pie._pre();
 
     var g = svg.selectAll(".arc")
-      .data(pie(data))
+      .data(pie(
+	options.map(e => data[e].length)
+      ))
       .enter().append("g")
       .attr("class", "arc");
 
@@ -102,12 +116,8 @@ class Pie {
 	return color(d.data)
       })
       .on("mouseover", (d, i) => {
-	let group = ["A", "B", "C", "D", "E"][i];
-	let _data = {};
-	_data.groups = [];
-	_data.groups[0] = raw.groups[i];
-	document.querySelector('#bar').innerHTML = '';
-	new Bar().score(_data);
+     	document.querySelector('#axis').innerHTML = '';
+     	Axis[chart](data[options[i]]);
       })
 
     g.append("text")
@@ -116,76 +126,54 @@ class Pie {
       })
       .attr("dy", ".35em")
       .text(function(d, i) {
-	return ["A", "B", "C", "D", "E"][i];
+	return options[i];
       })
   }
 
-  lunch(data) {
-    let raw = data;
-    data = data.lunch.map(e => e.length);
-
-    let {
-      svg, pie, arc, labelArc, color
-    } = Pie._pre();
-
-    var g = svg.selectAll(".arc")
-      .data(pie(data))
-      .enter().append("g")
-      .attr("class", "arc");
-
-    g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) {
-	return color(d.data)
-      })
-      .on("mouseover", (d, i) => {
-	let _data = {};
-    	_data.lunch = [];
-    	_data.lunch[0] = raw.lunch[i];
-    	document.querySelector('#bar').innerHTML = '';
-
-	if (i == 1) {
-	  new Bar().line(_data);
-	} else {
-	  new Bar().box(_data);
-	}
-      })
-
-    g.append("text")
-      .attr("transform", function(d) {
-	return "translate(" + labelArc.centroid(d) + ")";
-      })
-      .attr("dy", ".35em")
-      .text(function(d, i) {
-	return ["standard", "free/reduced"][i];
-      })
-  }
+  // static lunch(data, options) {
+  //   let raw = data;
+  //   data = data.lunch.map(e => e.length);
+  //   options = ["standard", "free/reduced"];
+  //   
+  //   let {
+  //     svg, pie, arc, labelArc, color
+  //   } = Pie._pre();
+  // 
+  //   var g = svg.selectAll(".arc")
+  //     .data(pie(data))
+  //     .enter().append("g")
+  //     .attr("class", "arc");
+  // 
+  //   g.append("path")
+  //     .attr("d", arc)
+  //     .style("fill", function(d) {
+  // 	return color(d.data)
+  //     })
+  //     .on("mouseover", (d, i) => {
+  // 	let _data = {};
+  //   	_data.lunch = [];
+  //   	_data.lunch[0] = raw.lunch[i];
+  //   	document.querySelector('#bar').innerHTML = '';
+  // 
+  // 	if (i == 1) {
+  // 	  new Bar().line(_data);
+  // 	} else {
+  // 	  new Bar().box(_data);
+  // 	}
+  //     })
+  // 
+  //   g.append("text")
+  //     .attr("transform", function(d) {
+  // 	return "translate(" + labelArc.centroid(d) + ")";
+  //     })
+  //     .attr("dy", ".35em")
+  //     .text(function(d, i) {
+  // 	return options[i];
+  //     })
+  // }
 }
 
-class Bar {
-  static _pre() {
-    let margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = 480 - margin.left - margin.right,
-      height = 250 - margin.top - margin.bottom;
-
-    // set the ranges
-    let x = d3.scaleBand()
-      .range([0, width])
-      .padding(0.1);
-    let y = d3.scaleLinear()
-      .range([height, 0]);
-
-    let svg = d3.select("#bar")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", 
-        "translate(" + margin.left + "," + margin.top + ")"
-      );
-
-    return { width, height, margin, x, y, svg };
-  }
-
+class Axis {
   static _averange(data) {
     let mt = 0;
     let rt = 0;
@@ -203,15 +191,42 @@ class Bar {
     
     return [ m, r, w ];
   }
+  
+  static _pre(data) {
+    let margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 480 - margin.left - margin.right,
+      height = 250 - margin.top - margin.bottom;
 
-  score(data) {
-    data = data.groups[0];
+    // set the ranges
+    let x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.1);
+    let y = d3.scaleLinear()
+      .range([height, 0]);
 
-    let { width, height, margin, x, y, svg } = Bar._pre();
-    let scores = Bar._averange(data);
+    let svg = d3.select("#axis")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", 
+        "translate(" + margin.left + "," + margin.top + ")"
+      );
+
+    let scores = Axis._averange(data);
     
     x.domain(['math', 'reading', 'writing']);
     y.domain([d3.min(scores) - 5, d3.max(scores) + 5]);
+
+    svg.append("g").call(d3.axisLeft(y));
+    svg.append("g").attr("transform",
+      "translate(0," + height + ")"
+    ).call(d3.axisBottom(x));
+
+    return { width, height, margin, x, y, svg, scores };
+  }  
+
+  static bar(data) {
+    let { width, height, margin, x, y, svg, scores} = Axis._pre(data);
 
     svg.selectAll(".bar")
       .data(scores)
@@ -219,34 +234,21 @@ class Bar {
       .style("fill", "steelblue")
       .attr("class", "bar")
       .attr("x", function(d, i) {
-	return x(['math', 'reading', 'writing'][i]);
+    	return x(['math', 'reading', 'writing'][i]);
       })
       .attr("width", x.bandwidth())
       .attr("y", function(d, i) {
-	return y(scores[i]);
+    	return y(scores[i]);
       })
       .attr("height", function(d, i) {
-	return height - y(scores[i]);
+    	return height - y(scores[i]);
       });
-
-    svg.append("g")
-      .attr("transform",
-	"translate(0," + height + ")"
-      )
-      .call(d3.axisBottom(x));
-
-    svg.append("g")
-      .call(d3.axisLeft(y))
   }
 
-  line(data) {
+  static line(data) {
     data = data.lunch[0];
 
-    let { width, height, margin, x, y, svg } = Bar._pre();
-    let scores = Bar._averange(data);
-    
-    x.domain(['math', 'reading', 'writing']);
-    y.domain([d3.min(scores) - 5, d3.max(scores) + 5]);
+    let { width, height, margin, x, y, svg, scores } = Axis._pre();
 
     let valueline = d3.line()
       .x(function(d, i) {
@@ -261,32 +263,13 @@ class Bar {
       .attr("class", "line")
       .attr("d", valueline)
       .attr("transform", `translate(${x.bandwidth() / 3 + margin.right}, 0)`)
-
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    svg.append("g")
-      .call(d3.axisLeft(y));
   }
 
-  box(data) {
-    data = data.lunch[0];
-
-    let { height, width, margin, x, y, svg } = Bar._pre();
-    let scores = Bar._averange(data);
+  static box(data) {
+    let { height, width, margin, x, y, svg, scores} = Axis._pre();
     
-    x.domain(['math', 'reading', 'writing']);
-    y.domain([d3.min(scores) - 5, d3.max(scores) + 5]);
-    
-    svg.append("g").call(d3.axisLeft(y));
-    svg.append("g").attr("transform",
-      "translate(0," + height + ")"
-    ).call(d3.axisBottom(x));
-
     width = 20;
-    let c = ['math', 'reading', 'writing'];
-    c.map((d, i) => {
+    options.map((d, i) => {
       let current = scores[i]
       let range = [current - 2, current, current + 2];
       
@@ -334,25 +317,31 @@ class Bar {
 
 /* score */
 function score() {
-  document.querySelector("#bar").innerHTML = '';
   document.querySelector("#pie").innerHTML = '';
+  document.querySelector("#axis").innerHTML = '';
   
-  let bar = new Bar();
   let pie = new Pie();
+  let axis = new Axis();
   
-  csv(bar.score);
-  csv(pie.score);
+  csv(Axis.bar, {
+    type: 'axis',
+    data: 'groupData',
+    init: true
+  });
+
+  csv(Pie.draw, {
+    type: 'pie',
+    data: 'groupData',
+    chart: 'bar',
+  });
 }
 
 function lunch() {
-  document.querySelector("#bar").innerHTML = '';
   document.querySelector("#pie").innerHTML = '';
-  
-  let bar = new Bar();
-  let pie = new Pie();
-  
-  csv(bar.box);
-  csv(pie.lunch);
+  document.querySelector("#axis").innerHTML = '';
+
+  csv(Pie.lunch);
+  csv(Axis.line);
 }
 
 /* Global */
