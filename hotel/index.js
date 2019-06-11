3915c8bcf7902fb389dc868abf7f4f3c1bdd912a
clearloop <udtrokia@gmail.com>
2019-06-11 12:38:54 +0800

    init hotel

---
+++ hotel/index.js
@@ -0,0 +1,18 @@
+;(function() {
+  /* Global Variables */
+  const dom = document;
+  function dqs(ident) { return dom.querySelector(ident) }
+  function dce(ident) { return dom.createElement(ident) }
+  
+  /* Fixed DOM */
+  const ut = dqs('#up-title');
+  const dt = dqs('#down-title');
+  const uc = dqs('#up-container');
+  const dc = dqs('#down-container');
+  
+  /* Home Page */
+  
+
+  /* Detail Page */
+  
+})();
