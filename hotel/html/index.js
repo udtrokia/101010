6844af4891c64a92424ef9d413bf16676522e753
clearloop <udtrokia@gmail.com>
2019-06-12 19:22:02 +0800

    add login window

--- hotel/html/index.js
+++ hotel/html/index.js
@@ -6,24 +6,20 @@
 
   function e(tag, _class, text, options = {attrs: {}, methods: {}}) {
     let el = document.createElement(tag);
-    
     el.setAttribute('class', _class);
 
-    // set attribute
     Object.entries(options.attrs).reduce(
       (element, [field, value]) => {
 	element.setAttribute(field, value);
 	return element;
       }, el);
 
-    // set methods
     Object.entries(options.methods).reduce(
       (element, [event, method]) => {
         element.addEventListener(event, method);
         return element;
       }, el);
 
-    // set text
     text?el.innerHTML = text:'';
     return el;
   }
@@ -34,37 +30,51 @@
   const uc = dqs('#up-container');
   const dc = dqs('#down-container');
 
+  /* Trigger */
+  ;(function() {
+    dqs('#login').addEventListener('click', function(e) {
+      dqs('#account').setAttribute('style', 'display: flex;')
+    });
+
+    dqs('.close').addEventListener('click', function(e) {
+      dqs('#account').setAttribute('style', 'display: none;')
+    });
+  })();
+
+  
+  
   /* Models */
   function card(data) {
     let _card = e('div', 'card');
     _card.appendChild(e('text', 'card-title', data.name));
     _card.appendChild(e('text', 'card-rate', data.rate));
-
-        
+    
     return _card;
   }
-
+  
   /* Home Page */
-  const hotels = [{
-    name: 'Omni Parker House',
-    rate: '4.2'
-  }, {
-    name: 'The Lenox Hotel',
-    rate: '4.6'
-  }, {
-    name: 'Mandarin Oriental',
-    rate: '4.6'
-  }, {
-    name: 'Fairmont Copley Plaza',
-    rate: '4.6'
-  }, {
-    name: 'The Eliot Hotel',
-    rate: '4.5'
-  }];
+  ;(function() {
+    const hotels = [{
+      name: 'Omni Parker House',
+      rate: '4.2'
+    }, {
+      name: 'The Lenox Hotel',
+      rate: '4.6'
+    }, {
+      name: 'Mandarin Oriental',
+      rate: '4.6'
+    }, {
+      name: 'Fairmont Copley Plaza',
+      rate: '4.6'
+    }, {
+      name: 'The Eliot Hotel',
+      rate: '4.5'
+    }];
 
-  hotels.map(h => {
-    uc.appendChild(card(h));
-  });
+    hotels.map(h => {
+      uc.appendChild(card(h));
+    });
+  })();
   
   /* Detail Page */
   
